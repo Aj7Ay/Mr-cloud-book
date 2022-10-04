962e1184fb3f2f6e96bfda645c44f14c47212350
Aj7ay <ajayyegireddi007@gmail.com>
2022-10-04 15:43:26 +0530

    First commit

---
+++ src/main/java/com/example/Ajmrcloud/AjMrcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ajmrcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjMrcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjMrcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ajmrcloud/AjMrcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ajmrcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjMrcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
